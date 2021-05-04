099f27b5f60223e45dba7bdfeed9f2def12af434
younggil Park <ygpark1230@gmail.com>
2021-05-04 16:11:26 +0900

    url whiteList  추가등록

--- rest_server/settings.py
+++ rest_server/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS = ["61.97.189.107", ".apiupass.com", "www.apiupass.com"]
+ALLOWED_HOSTS = ["61.97.189.107", ".apiupass.com", "www.apiupass.com","www.kyonggiupass.com"]
 #ALLOWED_HOSTS = ["101.101.218.36"]
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
