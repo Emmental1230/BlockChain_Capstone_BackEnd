37b526c519d11e88d068410debb8e911316fa5d7
younggil Park <ygpark1230@gmail.com>
2021-05-04 15:37:18 +0900

    runpython 오류찾기 시도

--- member/views.py
+++ member/views.py
@@ -111,7 +111,6 @@ def run_python(request):
                 json_data = json.load(f)
                 email = json_data['email']
                 did = json_data['did']
-                pwd2 = os.path.realpath(__file__)
             pwd3 = os.path.realpath(__file__)
             '''
             if (exitstatus==0): 
@@ -125,7 +124,7 @@ def run_python(request):
             '''
         except Exception as e: 
             result =  {"status": "failed_Exception"  , "output":str(e)} 
-            return JsonResponse({'msg':'failed_Exception','pwd1':pwd1,'pwd2':pwd2,'pwd3':pwd3}, status=400)
+            return JsonResponse({'msg':'failed_Exception','pwd1':pwd1,'pwd3':pwd3}, status=400)
 
         return JsonResponse(json_data, status=201)
 
