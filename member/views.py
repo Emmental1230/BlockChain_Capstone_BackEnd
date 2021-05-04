0a86daddb1c781377dd0e7bd73a3b897a0e24d9b
younggil Park <ygpark1230@gmail.com>
2021-05-04 15:44:37 +0900

    오류찾기

--- member/views.py
+++ member/views.py
@@ -101,17 +101,18 @@ def run_python(request):
         #command = ["python3","../docker/Blockchain_Capstone_Indy/start_docker/test_echo.py"]
         #command = ["sh","/home/caps/indy/start_docker/api.sh","f92f65a3731e","test@kyonggi.ac.kr"]
         #command = ["ls","-al","../docker/Blockchain_Capstone_Indy/start_docker/"]
-        command = ["sh","../../indy/start_docker/api.sh","f92f65a3731e","test@kyonggi.ac.kr"]
+        command = ["sh","/home/caps/indy/start_docker/api.sh","f92f65a3731e","test@kyonggi.ac.kr"]
         pwd3 = os.path.realpath(__file__)
         try: 
             process = Popen(command, stdout=PIPE, stderr=STDOUT) 
             output = process.stdout.read() 
             exitstatus = process.poll() 
-            pwd1 = os.path.realpath(__file__)
-            with open('../..indy/start_docker/data.json')as f:
+            
+            with open('../../indy/start_docker/data.json')as f:
                 json_data = json.load(f)
                 email = json_data['email']
                 did = json_data['did']
+            pwd1 = os.path.realpath(__file__)
             '''
             if (exitstatus==0): 
                 with open('./data.json')as f:
@@ -124,7 +125,7 @@ def run_python(request):
             '''
         except Exception as e: 
             result =  {"status": "failed_Exception"  , "output":str(e)} 
-            return JsonResponse({'msg':'failed_Exception','pwd3':pwd3}, status=400)
+            return JsonResponse({'msg':'failed_Exception','pwd3':pwd1}, status=400)
 
         return JsonResponse(json_data, status=201)
 
