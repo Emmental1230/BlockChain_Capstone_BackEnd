1e9c94a14ef9a34d6c5cc2f93cb3f92e638e85dd
younggil Park <ygpark1230@gmail.com>
2021-05-05 15:47:00 +0900

    python 에러 수정

--- member/views.py
+++ member/views.py
@@ -116,31 +116,3 @@ def readDID(request):
         email = json_data['email']
         did = json_data['did']
     return JsonResponse(json_data, status=201)
-
-
-@csrf_exempt
-def findMyInfo(request):
-    #가입된 회원이 맞는지 확인
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        studentDB = Member.objects.all()
-        
-        student = Member(
-            stdnum = data['stdnum']
-            email = data['email']
-        )
-        email = data['email']
-        stdnum = data['stdnum']
-        
-        if studentDB.filter(email = email).exists() :
-            return JsonResponse({'msg':'Invalid email entered '}, status=400)
-        elif studentDB.filter(stdnum = stdnum).exists() :
-            return JsonResponse({'msg':'Invalid student_ID entered'}, status=400)
-        else:
-            email_dump = json.dumps(email, sort_keys = True).encode() #기본 이메일 str으로 저장
-            email_hash = hashlib.sha256(email_dump).hexdigest() # 이메일 해싱 저장
-            email_data_json = { 'email' : '' }  #email_data_json 생성
-            email_data_json['email'] = email_hash   #해싱된 것을 json으로 저장
-            data['email'] = email_hash
-
-        return JsonResponse(email_data_json, status=201)
