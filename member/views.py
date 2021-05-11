9e9df507908bece30144c2d167188ad308b0da06
younggil Park <ygpark1230@gmail.com>
2021-05-11 16:30:48 +0900

    findmyinfo 함수 수정 ( stdnum이 없는경우에도 돌아가도록 수정)

--- member/views.py
+++ member/views.py
@@ -62,8 +62,8 @@ def member_list(request):
         if serializer.is_valid():       #입력 data들 포맷 일치 여부 확인
             serializer.save()
             return JsonResponse(email_data_json, status=201)
-
-        return JsonResponse(serializer.errors, status=400)       
+        else:
+            return JsonResponse({'msg' :'data format이 일치하지 않습니다.'}, status=400)
 
 
 @csrf_exempt
@@ -73,15 +73,7 @@ def member(request, word):
 
     if request.method == 'GET':
         serializer = MemberSerializer(obj)
-        return JsonResponse(serializer.data, status=201, safe=False)
-    
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = MemberSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)   
+        return JsonResponse(serializer.data, status=201, safe=False)  
 
 
 @csrf_exempt
@@ -99,8 +91,6 @@ def run_python(request):
                 did = json_data['did']
             
                 #result = {"status": "Failed  ", "output":str(output)}
-
-
         except Exception as e: 
             #result =  {"status": "failed_Exception"  , "output":str(e)} 
             return JsonResponse({'msg':'failed_Exception','erreor 내용':str(e),'pwd':pwd}, status=400)
@@ -124,17 +114,16 @@ def findmyinfo(request):
     if request.method == 'POST':
         data = JSONParser().parse(request)
         studentDB = Member.objects.all()
-        #email = "1544455cac2644c63b59551bc9e135a38cd9b59ba2ca4f475d45a41737bf3f62"
-        #stdnum = "12341234"
-
+        studentVals = Member.objects.values()
+        
+        #email 정보가 DB에 있는지 확인
         if studentDB.filter(email = data['email']).exists() :
-            email = studentDB.filter(email = data['email'])
-            stdnum = studentDB.filter(stdnum = data['stdnum'])
-            if email[0]==stdnum[0] :
-                #email 과 stdnum이 한 사람의 정보일 경우 진행
-                #반환값을 만들고 해당 값을 반환하면됨
-                return JsonResponse({'email':email_hash}, status=201)
+            stdinfo = studentVals[0]['id']     #있다면, 해당 학생 id 저장
+            #stdnum부분이 공란일 경우 or
+            #stdnum가 있고, email, stdnum이 한사람의 정보일 경우
+            if not data['stdnum']  or Member.objects.get(id=stdinfo).stdnum == data['stdnum'] :
+                return JsonResponse({'email_hash':Member.objects.get(id=stdinfo).email_hash})
             else :
                 return JsonResponse({'msg':'email과 stdnum이 일치하지 않습니다.'}, status=400)
-        else :
+        else :  
             return JsonResponse({'msg': '가입되지 않은 email입니다.'}, status=400)
