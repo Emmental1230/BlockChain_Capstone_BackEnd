f4db512c9d46a589b243ad7f48552a63057254ad
younggil Park <ygpark1230@gmail.com>
2021-05-12 16:47:01 +0900

    제발

--- member/views.py
+++ member/views.py
@@ -76,7 +76,7 @@ async def run_python(request):
         email = request.GET.get('email', None)
         simple_pw = request.GET.get('SimplePassword', None)
 
-        command = ["sh","/home/caps/indy/start_docker/api.sh","ed1ff7a2fc14 " + email + " " + simple_pw]
+        command = ["sh","/home/caps/indy/start_docker/api.sh","ed1ff7a2fc14 ", email]
         # command = "sh /home/caps/indy/start_docker/api.sh ed1ff7a2fc14 " + email +" "+ simple_pw
         try:
             process = Popen(command, stdout=PIPE, stderr=STDOUT)
