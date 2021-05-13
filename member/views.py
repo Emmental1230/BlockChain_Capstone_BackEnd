43135224aa12d44b2f952a4ee0b47dde2f6edaaf
ryantutoring <ryan.r@mdcrew.co.kr>
2021-05-13 12:33:56 +0900

    test

--- member/views.py
+++ member/views.py
@@ -75,9 +75,9 @@ async def run_python(request):
     if request.method == 'POST':
         email = request.GET.get('email', None)
         simple_pw = request.GET.get('SimplePassword', None)
-        # command = ["/bin/sh","../indy/start_docker/api.sh","1b57c8002249", email, simple_pw]
+        command = ["/bin/sh","../indy/start_docker/api.sh","1b57c8002249", email, simple_pw]
         # command = ["docker","exec","-itu", "0", "1b57c8002249", "python3 /home/indy/generate_did.py",  ]
-        command = 'docker exec -itu 0 1b57c8002249 python3 /home/indy/generate_did.py 111 111'.split()
+        # command = 'docker exec -itu 0 1b57c8002249 python3 /home/indy/generate_did.py 111 111'.split()
         print("command: ", command)
         print("email: ", email)
         print("simple_pw:", simple_pw)
