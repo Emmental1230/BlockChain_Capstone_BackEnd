999e86503bad1b5e4a73670e4fee7ae6eb585042
ryantutoring <ryan.r@mdcrew.co.kr>
2021-05-13 12:53:33 +0900

    test

--- member/views.py
+++ member/views.py
@@ -88,8 +88,8 @@ async def run_python(request):
             # process = Popen(command, stdout=PIPE)
 
             process.wait()
-            #output = process.stdout.read()
-            output, err = process.communicate()
+            output = process.stdout.read()
+            # output, err = process.communicate()
             print("output:", output)
 #            exitstatus = process.poll()
 #            pwd = os.path.realpath(__file__)
@@ -107,8 +107,8 @@ async def run_python(request):
         except Exception as e:
             #result =  {"status": "failed_Exception"  , "output":str(e)}
             return JsonResponse({'msg':'failed_Exception','erreor 내용':str(e)}, status=400)
-        return JsonResponse(json_data, status=201)
-        #return JsonResponse({'output':str(output.decode()), 'err':str(err),'command':str(command), 'email':email, "simple_pw":simple_pw, 'did':did}, status=201)
+        # return JsonResponse(json_data, status=201)
+        return JsonResponse({'output':str(output), 'command':str(command), 'email':email, "simple_pw":simple_pw, 'did':did}, status=201)
 
 @csrf_exempt
 def findmyinfo(request):
